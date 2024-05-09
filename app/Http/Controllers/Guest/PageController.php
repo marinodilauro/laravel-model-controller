c189029de937b74bad60993c66ee8b55e24bf23e
marinodilauro <m.dilauro90@gmail.com>
2024-05-09 14:22:29 +0200

    Add movie cards on screen

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $movies = Movie::all();
+        // dd($books);
+        return view('welcome', compact('movies'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,36 @@
 
 
 @section('content')
-  <h1>HomePage</h1>
+  <div class="container">
+
+    <h1>HomePage</h1>
+
+    <div class="row row-cols-2 row-cols-md-4 g-4">
+
+      @forelse ($movies as $movie)
+        <div class="col">
+          <div class="card">
+            <div class="card-header">
+              <h3 class="card-title">{{ $movie->title }}</h3>
+              <h4>{{ $movie->original_title }}</h4>
+            </div>
+            <div class="card-body">
+              <ul class="list-unstyled">
+                <li>Nationality: {{ $movie->nationality }}</li>
+                <li>Release date: {{ $movie->date }}</li>
+                <li>Vote: {{ $movie->vote }}/10</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      @empty
+
+        <div class="col">
+          <p>No movies here, sorry 😓</p>
+        </div>
+      @endforelse
+
+    </div>
+  </div>
 @endsection
